5c2bc9bfd7a3d0c093abbe7e30e160c55563d312
Gags <vgagan971@gmail.com>
2021-09-26 22:43:52 +0530

    random

--- hello.java
+++ hello.java
@@ -6,6 +6,7 @@ public class hello {
      public static void main(String[] args)  {
        String str="hello gags";
        System.out.println(str);   
-     
+       System.out.println(str);   
+
     }
   }
